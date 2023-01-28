9da048231976a0120fb6a0e5a8d46d617d930523
Mark <datawithmark@gmail.com>
2023-01-28 22:40:48 +0530

    Day 1 of JS

---
+++ Day 01/Exercises.js
@@ -0,0 +1,50 @@
+// Question 1 to 3
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/**
+ * comments can make code readable,
+ * easy to reuse and informative
+ */
+
+// Question 4: Declare variables and assign string, boolean, undefined and null data types
+let intVar = 3;
+let floatVar = 3.14;
+let stringVar = "hello"; // 'hello' or `hello`
+let undefinedVar;
+let nullVar = null;
+
+// Question 5: Use the JavaScript typeof operator to check different data types. Check the data type of each variable.
+console.log(typeof intVar);
+console.log(typeof floatVar);
+console.log(typeof (stringVar));
+console.log(typeof (undefinedVar));
+console.log(typeof nullVar);
+
+// Question 6: Declare four variables without assigning values
+firstVariable
+second_Variable
+_num_1
+$num
+
+// Quetion 7: Declare four variables with assigned values
+let myName = 'Mark';
+let bio = `I am
+learning
+Web Dev.`;
+var my_age = 21;
+const cuntry = "India";
+
+// Question 8: Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Mark";
+let lastName = "Albatross";
+let maritalStatus = false;
+let myCountry = "India";
+let Age = 21;
+
+// Question 9: Declare variables to store your first name, last name, marital status, country and age in a single line
+let first_Name = "Mark", last_Name = "Albatross", marital_status = false, my_country = "India", age = 21;
+console.log(first_Name, last_Name, marital_status, my_country, age);
+
+// Question 10: Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 22, yourAge = 25;
+console.log("I am " + myAge + " years old.\nYou are " + yourAge + " years old.");
